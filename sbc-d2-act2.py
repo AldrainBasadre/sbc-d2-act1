2de12dfa17465fd4ab4d23b0a562494ee19a7a0a
CarlMichaelFabe-ACLC <132441925+CarlMichaelFabe-ACLC@users.noreply.github.com>
2024-06-19 15:57:33 +0800

    version2

---
+++ sbc-d2-act2.py
@@ -0,0 +1,30 @@
+from random import randint
+
+me = int(input("enter o hayang to  1 kulob: "))
+c2 = randint(0,1)
+c3 = randint(0,1)
+
+if me == 1:
+    print("Me:kulob")
+elif me == 0:
+    print("Me:hayang")
+if c2 == 1:
+    print("C2:kulob")
+elif c2 == 0:
+    print("C2:hayang")
+if c3 == 1:
+    print("C3:kulob")
+elif c3 == 0:
+    print("C3:hayang")
+    
+
+
+if me == 0 and c2 == 1 and c3 == 1 or me == 1 and c2 == 0 and c3 == 0 :
+    print("you win")
+elif me == 1 and c2 == 0 and c3 == 1 or me == 0 and c2 == 1 and c3 == 0:
+    print("c2 win")
+elif me == 1 and c2 == 1 and c3 == 0 or me == 0 and c2 == 0 and c3 == 1:
+    print("c2 win")
+else:
+    print("draw")
+    
