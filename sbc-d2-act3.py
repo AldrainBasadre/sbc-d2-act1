5ec855948798ff2e11d9a54ddb83090dd1e3d158
CarlMichaelFabe-ACLC <132441925+CarlMichaelFabe-ACLC@users.noreply.github.com>
2024-06-19 21:47:03 +0800

    version4

--- sbc-d2-act3.py
+++ sbc-d2-act3.py
@@ -6,9 +6,9 @@ first = int(input("please enter the first number: "))
 second = int(input("please enter the second number: "))
 third = int(input("please enter the third number: "))
 
-Lfnum = random.randint(1, 3)
-Lsnum = random.randint(1, 2)
-Ltnum = random.randint(2, 3)
+Lfnum = random.randint(0, 9)
+Lsnum = random.randint(0, 9)
+Ltnum = random.randint(0, 9)
 
 mylist = first,second,third
 lotolist = Lfnum,Lsnum,Ltnum
